c93be8f6c420b8581ff0a92afe1fab767633516d
Akito Mimura <mimuraakkun3@gmail.com>
2020-07-04 21:54:10 +0900

    :+1: firebase/authをImport

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,27 +1,20 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-//import firebaseui from 'firebaseui'
+import * as firebase from 'firebase/app'
+import "firebase/auth"
+import "firebase/firestore"
 
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: "AIzaSyC6qBEhqet5sCNRw_Dm-7FEfNPPSaHhLRU",
-    authDomain: "sample-4343c.firebaseapp.com",
-    databaseURL: "https://sample-4343c.firebaseio.com",
-    projectId: "sample-4343c",
-    storageBucket: "sample-4343c.appspot.com",
-    messagingSenderId: "517839382746",
-    appId: "1:517839382746:web:921d4f5e1903a8ef751de3"
-  })
+var firebaseConfig = {
+  //firebase構成オブジェクト
+  apiKey: "AIzaSyC6qBEhqet5sCNRw_Dm-7FEfNPPSaHhLRU",
+  authDomain: "sample-4343c.firebaseapp.com",
+  databaseURL: "https://sample-4343c.firebaseio.com",
+  projectId: "sample-4343c",
+  storageBucket: "sample-4343c.appspot.com",
+  messagingSenderId: "517839382746",
+  appId: "1:517839382746:web:921d4f5e1903a8ef751de3"
 }
-
-//var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-// ui.start('#firebaseui-auth-container', {
-//   signInOptions: [{
-//     provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//     requireDisplayName: true
-//   }]
-// });
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
 
 const db = firebase.firestore()
 const postRef = db.collection('posts')
