303d6e57f17074908b765bd7f582940cd5b04bd7
Akito Mimura <mimuraakkun3@gmail.com>
2020-07-08 23:14:31 +0900

    :tada: ユーザーのマイページを作成

---
+++ pages/users/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-content>
+    <v-card width="1000px" class="mx-auto my-10">
+      <v-card-title>{{ userData.name }}</v-card-title>
+      <v-card-subtitle>{{ userData.email}}</v-card-subtitle>
+      <v-card-text></v-card-text>
+      <v-card-actions></v-card-actions>
+    </v-card>
+  </v-content>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  computed: {
+    userData() {
+      return this.$store.state.auth.data
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
